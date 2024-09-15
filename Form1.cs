43a510e51ba272f303ab04cf73facef785f657a5
AlexanderZemlyak <zemlyaksanya@mail.ru>
2024-09-15 10:37:54 +0300

    Implement first task

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CG_Lab2
@@ -15,6 +8,12 @@ namespace CG_Lab2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormTask1 task1Form = new FormTask1();
+            task1Form.Show();
         }
     }
 }
